9e8f411355c839efdb0d57a7772b705e0707ab81
Vessy Stag <vesselina.agova@gmail.com>
2014-06-11 22:18:50 +0300

    Rock Paper Scissors Game v.1.2
    Now you can play against the pc.
    + better "view" by playing between two people

--- game/RockPaperScissors.rb
+++ game/RockPaperScissors.rb
@@ -1,29 +1,53 @@
-puts "Rock Paper Scissors?"
-answer1 = gets.chomp
-if answer1 != "Rock" and answer1 != "Paper" and answer1 != "Scissors"
+puts "Two of you or you against me?"
+answer = gets.chomp.capitalize
+
+turns = ['Rock', 'Paper', 'Scissors']
+
+if answer == "Two" and answer == "Two of us"
+  puts "Rock Paper Scissors?"
+  answer1 = gets.chomp.capitalize
+unless turns.include? answer1
   puts "Incorrect try"
   exit
 end
 
+100.times {puts ""}
+
 puts "Rock Paper Scissors?"
-answer2 = gets.chomp
-if answer2 != "Rock" and answer2 != "Paper" and answer2 != "Scissors"
+answer2 = gets.chomp.capitalize
+unless turns.include? answer2
   puts "Incorrect try"
   exit
+end
+else
+  puts "You first: Rock Paper Scissors?"
+  answer1 = gets.chomp.capitalize
+unless turns.include? answer1
+  puts "Incorrect try"
+  exit
+end
+answer2 = turns.shuffle.sample
+puts "My turn, human! I play #{answer2}"
 end
 
 if answer1 == "Rock" and answer2 == "Scissors" then
-  puts "Player 1 wins"
+  puts "Player 1 played #{answer1}. Player 1 wins!"
 elsif answer1 == "Rock" and answer2 == "Paper" then
-  puts "Player 2 wins"
+  puts "Player 1 played #{answer1}. Player 2 wins!"
 elsif answer1 == "Paper" and answer2 == "Rock" then
-  puts "Player 1 wins"
+  puts "Player 1 played #{answer1}. Player 1 wins!"
 elsif  answer1 == "Paper" and answer2 == "Scissors" then
-  puts "Player 2 wins"
+  puts "Player 1 played #{answer1}. Player 2 wins!"
 elsif answer1 == "Scissors" and answer2 == "Paper" then
-  puts "Player 1 wins"
+  puts "Player 1 played #{answer1}. Player 1 wins!"
 elsif answer1 == "Scissors" and answer2 == "Rock" then
-  puts "Player 2 wins"
+  puts "Player 1 played #{answer1}. Player 2 wins!"
 else
-  puts "You lose. I mean, both of you"
+  puts "We are even."
 end
+
+
+
+
+
+
