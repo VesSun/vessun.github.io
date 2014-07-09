ed858b8e39f329c1fe59c805f395d4cf01b6cf45
Vessy Stag <vesselina.agova@gmail.com>
2014-07-09 21:12:33 +0300

    en-de dictionary
    new:
    loop
    file.read

--- ruby experiments/Dict.rb
+++ ruby experiments/Dict.rb
@@ -1,32 +1,36 @@
 # encoding: utf-8
+en_de_dict = {}
+
+raw_dict = File.read('en_de_dict')
+dict_lines = raw_dict.split("\n")
+dict_lines.each do |line|
+  word_pair = line.strip.split(',')
+  en_word = word_pair[0]
+  de_word = word_pair[1]
+  en_de_dict[en_word] = de_word
+end
 
 puts "Gimme a word to translate"
-answer = gets.chomp.capitalize
+loop do 
+  word = gets.chomp.capitalize
+  if word == ""
+    break
+  end
+  if en_de_dict.has_key?(word)
+    puts "Word in DE: #{en_de_dict[word]}"
+  else
+    puts "No word in the dict"
+  end
 
-dict = {
-  'Rock' => 'Камък',
-  'Paper' => 'Хартия',
-  'Scissors' => 'Ножица',
-}
+  de_en_dict = en_de_dict.invert
 
-if dict.has_key?(answer)
-  puts "Word in BG: #{dict[answer]}"
+  if de_en_dict.has_key?(word)
+    puts "Word in EN #{de_en_dict[word]}"
 
-else
-  puts "Беден ми е тако'ата ;)"
+  else
+    puts "Keine solche Wort"
+  end
+
 end
-
-dict1 = dict.invert
-
-print dict1
-
-if dict1.has_key?(answer)
-  puts "Ма 'земи да бачкаш най-накрая! #{dict[answer]}"
-
-else
-  puts "AAaaaaa noo"
-end
-
-
 
 
