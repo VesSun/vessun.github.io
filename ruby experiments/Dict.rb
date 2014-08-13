d3e51d7e8b680c275a5c7e80e7a0c9fa81b3f470
Vessy Stag <vesselina.agova@gmail.com>
2014-08-13 20:19:44 +0300

    en-de dict
    some better things

--- ruby experiments/Dict.rb
+++ ruby experiments/Dict.rb
@@ -10,40 +10,57 @@ dict_lines.each do |line|
   en_de_dict[en_word] = de_word
 end
 
+ de_en_dict = en_de_dict.invert
+
 puts "Gimme a word to translate"
 loop do 
+  print "> "
   input = gets.chomp
   if input == ""
     break
   end
-  words = input.split(' ')
+
+
   translated_words = []
 
+  words = input.split(' ')
   words.each do |word|
-    word = word.capitalize
+  word = word.capitalize
+  end
 
-    if en_de_dict.has_key?(word)
-      translated_words.push(en_de_dict[word])
-    else
-      translated_words.push(word)
+  
+  if words.include?('(en)')
+    words.delete('(en)')
+    words.each do |word|
+      word = word.capitalize
+      if en_de_dict.has_key?(word)
+        translated_words.push(en_de_dict[word])
+      elsif
+        translated_words.push(word)
+      end
     end
 
+  elsif 
+    words.include?('(de)')
+    words.delete('(de)')
+    words.each do |word|
+      word = word.capitalize
+      if de_en_dict.has_key?(word)
+         translated_words.push(de_en_dict[word])
+      elsif
+        translated_words.push(word)
+      end
+    end
+  else 
+    words.each do |word|
+      word = word.capitalize
+      if en_de_dict.has_key?(word)
+        translated_words.push(en_de_dict[word])
+      elsif
+        translated_words.push(word)
+      end
+    end
   end
 
   puts translated_words.join(' ')
-  
-  
- 
-
-  # de_en_dict = en_de_dict.invert
-
-  # if de_en_dict.has_key?(word)
-  #   puts "Word in EN #{de_en_dict[word]}"
-
-  # else
-  #   puts "Keine solche Wort"
-  # end
-
 end
-
-
